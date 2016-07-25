231869660ffdffc3eea35379c2eb64d3b0c62d94
Aaron Dotson <addotson@alaska.edu>
2016-07-25 11:56:35 -0800

    corrected spelling compile

--- valve_schedule.py
+++ valve_schedule.py
@@ -1,13 +1,13 @@
 '''
 David Rodriguez
 
-Goal: Continuously looping while to perform valve actions at specified times, 
-introduce substance at a specific ratio based on flow data, recording 
+Goal: Continuously looping while to perform valve actions at specified times,
+introduce substance at a specific ratio based on flow data, recording
 and saving flow data, and actuating a flush at a specified time.
 
 Inputs: A schedule of events based on entered times.
 
-Outputs: Sequence of events to a screen as they happen. 
+Outputs: Sequence of events to a screen as they happen.
 daily flow rate data
 '''
 #Handles all scheduling related tasks
@@ -16,11 +16,11 @@ import datetime
 import re
 
 class Schedule:
-    
+
     def __init__(self):
         self.eventList = []
         self.eventCount = 0
-        
+
     def displayMenu(self):
         print "\n1: Import Existing Schedule"
         print "2: Add Event"
@@ -28,27 +28,27 @@ class Schedule:
         print "4: Display Events"
         print "5: Save Schedule"
         print "6: Return to main menu"
-        
+
     def addEvent(self):
-        r = re.complie('[0-2]\d:[0-5]\d:[0-5]\d')
+        r = re.compile('[0-2]\d:[0-5]\d:[0-5]\d')
         matches = False
         while matches == False:
             enteredTime = raw_input("Please enter the time you want the event to "
                                     "occur. Format as HH:MM:SS (eg 15:30:12) ")
             if r.match(enteredTime) is not None:
                 matches = True
-                
+
         start = datetime.time()
         start = datetime.datetime.strptime(enteredTime, "%H:%M:%S").time()
         datetime.time(3, 55, 55)
-        
+
         matches = False
         while matches == False:
             enteredTime = raw_input("Please enter the time you want to stop "
                                     "the event. Format as HH:MM:SS (eg 15:31:06) ")
             if r.match(enteredTime) is not None:
                 matches = True
-                
+
         end = datetime.time()
         end = datetime.datetime.strptime(enteredTime, "%H:%M:%S").time()
         datetime.time(3, 55, 55)
@@ -69,19 +69,19 @@ class Schedule:
                 print "The was not a valid option please try again.\n"
 
         new = Event(start, end, tag)
-        
+
         self.eventList.append(new)
         self.eventCount += 1
         print "\nEvent added."
         new.displayEvent()
-    
+
     def displaySchedule(self):
         self.eventList.sort(key = lambda event: event.startTime)
         counter = 1
         for event in self.eventList:
             print str(counter) + ": " + event.displayEvent()
             counter += 1
-            
+
     def saveSchedule(self):
         self.eventList.sort(key = lambda event: event.startTime)
         filename = raw_input("Please enter a name for the Schedule.\n")
@@ -91,7 +91,7 @@ class Schedule:
             target.write("\n")
         target.close()
         print "Schedule saved as %s" % filename
-        
+
     def importSchedule(self):
         filename = raw_input("\nType the name of the schedule you want to import.\n")
         with open(filename, 'r') as target:
@@ -101,17 +101,17 @@ class Schedule:
                 minute = int(start.split(":")[1])
                 second = int(start.split(":")[2])
                 start = datetime.time(hour, minute, second)
-                
+
                 end = line.split(" ")[1]
                 hour = int(end.split(":")[0])
                 minute = int(end.split(":")[1])
                 second = int(end.split(":")[2])
                 end = datetime.time(hour, minute, second)
-                
+
                 tag = line.split(" ")[2]
-                
+
                 new = Event(start, end, tag)
-                
+
                 self.eventList.append(new)
                 self.eventCount += 1
         target.close()
